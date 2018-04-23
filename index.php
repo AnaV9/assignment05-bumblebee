85ab03e8a5a91bfbfe17ff0853f3bba8182a32d1
Ana <avaquera@u.rochester.edu>
2018-04-23 03:21:49 -0400

    edit

--- index.php
+++ index.php
@@ -9,10 +9,6 @@
 		<div class = "slider"> 
 			<img src="images/flag.png" alt="Mexican Flag">
 			<img src="images/mexico-city.jpg" alt="Mexico City">
-			<img src="images/history.jpg" alt="Pre-Columbian Mexico">
-			<img src="images/tamale.jpg" alt="Mexican Tamales">
-			<img src="images/dance.jpg" alt="Dance">
-			<img src="images/women.jpg" alt="Women in Mexico">
 		</div>
 
 		<article>
@@ -20,6 +16,7 @@
 			<p>Pre-Columbian Mexico dates to approximately 8,000 BC, is identified as one of seven cradles of civilization and was home to many advanced Mesoamerican civilizations, such as the Olmec, Toltec, Teotihuacan, Zapotec, Maya and Aztec before first contact with Europeans.</p><p><a class="readmore" href = "history.php"> Read More</a></p>
 		<h2>Culture</h2>
 			<p>Mexican culture reflects the complexity of the country's history through the blending of indigenous cultures and the culture of Spain, imparted during Spain's 300-year colonization of Mexico. Exogenous cultural elements have been incorporated into Mexican culture as time has passed.</p><p><a class="readmore" href = "culture.php"> Read More</a></p>
+		<p>Take a survery!</p>
 		</article>
 
 		<?php include "inc/footer.inc"; ?>
--- welcome.php
+++ welcome.php
@@ -1,49 +1,28 @@
 <?php
-	// 1. Create a database connection
-	$dbhost = "128.151.150.1";
-	$dbuser = "urcscon3_avaquer";
-	$dbpass = "coffee1";
-	$dbname = "urcscon3_avaquera";
-	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-	// Usually this data would come from HTML form values in $_POST
-	$email = Trim(stripslashes($_POST['email']));
-	
-	// You really should escape all strings
-	//$insertData = mysqli_real_escape_string($connection, $insertData);
-	
-	// 2. Perform database query
-	$query  = "INSERT INTO myTable (email) VALUES ('$email')";
-	$result = mysqli_query($connection, $query);
-
+// Initialize the session
+session_start();
+ 
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: login.php");
+  exit;
+}
 ?>
-
-<?php include "inc/html-top.inc";?>
-	<body>
-	<section class = "banner">
-		<?php include "inc/header.inc"; ?>
-		<?php include "inc/nav.inc"; ?>
-	</section>
-
-		<div class= "container">
-
-		<article class = "welcome">
-		<h2>Thank you!</h2>
-			<p>You have been added to our mailing list.</p>
-			<p><a href = "index.php">Read More About Mexico</a></p>
-		</article>
-
-		<?php include "inc/footer.inc"; ?>
-		</div> <!--.container -->
-		<?php include "inc/scripts.inc"; ?>
-	</body>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
+</body>
 </html>
-
-<?php
-	// 4. Step 4 is unnecessary here because we didn't 
-	//	  get data that needs to be released
-	//mysqli_free_result($result);
-
-	// 5. Close database connection
-	mysqli_close($connection);
-?>
